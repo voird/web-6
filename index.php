f3e5c91890652afd4938423b02fe617539f99adf
polina_storozh <58592174+voird@users.noreply.github.com>
2023-05-28 21:22:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,10 +11,10 @@ try{
     $get=$db->prepare("select password from admin where name=?");
     $get->execute(array('admin'));
     $pass_hash=$get->fetchAll()[0][0];
-   // echo '<pre>';
+    // echo '<pre>';
     //print_r($pass_hash);
-   // var_dump($pass_hash);
-   // echo '</pre>';
+    // var_dump($pass_hash);
+    // echo '</pre>';
 }
 
 catch(PDOException $e){
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (!empty($_COOKIE['save'])) {
         setcookie('save', '', 100000);
         $messages[] = 'Спасибо, результаты сохранены.';
-        setcookie('name_value', '', 100000);
+        setcookie('fio_value', '', 100000);
         setcookie('email_value', '', 100000);
         setcookie('year_value', '', 100000);
         setcookie('gender_value', '', 100000);
@@ -67,7 +67,7 @@ if (empty($_SERVER['PHP_AUTH_USER']) ||
         
         $errors = array();
         $error=FALSE;
-        $errors['name'] = !empty($_COOKIE['name_error']);
+        $errors['fio'] = !empty($_COOKIE['fio_error']);
         $errors['email'] = !empty($_COOKIE['email_error']);
         $errors['year'] = !empty($_COOKIE['year_error']);
         $errors['gender'] = !empty($_COOKIE['pol_error']);
@@ -75,9 +75,9 @@ if (empty($_SERVER['PHP_AUTH_USER']) ||
         $errors['superpower'] = !empty($_COOKIE['superpower_error']);
         $errors['text'] = !empty($_COOKIE['text_error']);
         $errors['check'] = !empty($_COOKIE['check_error']);
-        if ($errors['name']) {
-            setcookie('name_error', '', 100000);
-            $messages[] = '<div class="error">Заполните имя или у него неверный формат (only English)</div>';
+        if ($errors['fio']) {
+            setcookie('fio_error', '', 100000);
+            $messages[] = '<div class="error">Заполните имя</div>';
             $error=TRUE;
         }
         if ($errors['email']) {
@@ -121,40 +121,31 @@ if (empty($_SERVER['PHP_AUTH_USER']) ||
             $get->bindParam(1,$id);
             $get->execute();
             $inf=$get->fetchALL();
-            $values['name']=$inf[0]['name'];
+            $values['fio']=$inf[0]['name'];
             $values['email']=$inf[0]['email'];
             $values['year']=$inf[0]['year'];
             $values['gender']=$inf[0]['pol'];
             $values['limbs']=$inf[0]['limbs'];
             $values['text']=$inf[0]['bio'];
-            $values['superpower'] = 't';
+            $values['superpower'] = '';
             $values['check'] = TRUE;
-           // echo '<pre>';
-          //  print_r($inf);
-          //  var_dump($inf);
-          ////  echo '</pre>';
-            $get2=$db->prepare("SELECT * FROM Sform WHERE id_per=?");
+            
+            $get2=$db->prepare("SELECT id_sup FROM Sform WHERE id_per=?");
             $get2->bindParam(1,$id);
             $get2->execute();
-            foreach ($get2 as $row){
-                
-            }
             $inf2=$get2->fetchALL();
-        //    echo '<pre>';
-        //     print_r($inf2);
-         //     echo '</pre>';
-            if($inf2 == '10'){
-            $values['superpower']='t';
+            if($inf2[0]['id_sup']=='10'){
+                $values['superpower'] == 't';
             }
-            if($inf2 == '20'){
-            $values['superpower']='b';
+            if($inf2[0]['id_sup']=='20'){
+                $values['superpower'] == 'b';
             }
-            if($inf2 == '30'){
-            $values['superpower']='c';
+            if($inf2[0]['id_sup']=='30'){
+                $values['superpower'] == 'c';
             }
-            if($inf2 == '40'){
-            $values['superpower']='p';
-           }
+            if($inf2[0]['id_sup']=='40'){
+                $values['superpower'] == 'p';
+            }
         }
         catch(PDOException $e){
             print('Error: '.$e->getMessage());
@@ -174,53 +165,76 @@ if (empty($_SERVER['PHP_AUTH_USER']) ||
             $bio=$_POST['text'];
             
             //Регулярные выражения
-            $bioregex = "/^\s*\w+[\w\s\.,-]*$/";
-            $nameregex = "/^\w+[\w\s-]*$/";
-            $mailregex = "/^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/";
             $errors = FALSE;
-            
-            if (empty($name) || (!preg_match($nameregex,$name))) {
-                setcookie('name_error', '1', time() + 24*60 * 60);
-                setcookie('name_value', '', 100000);
+            if (empty($_POST['fio'])) {
+                // Выдаем куку на день с флажком об ошибке в поле fio.
+                setcookie('fio_error', '1', time() + 24 * 60 * 60);
                 $errors = TRUE;
             }
+            else {
+                // Сохраняем ранее введенное в форму значение на месяц.
+                setcookie('fio_value', $_POST['fio'], time() + 30 * 24 * 60 * 60);
+            }
             
-            if (empty($email) || !filter_var($email,FILTER_VALIDATE_EMAIL) ||
-                (!preg_match($mailregex,$email))) {
-                    setcookie('email_error', '1', time() + 24*60 * 60);
-                    setcookie('email_value', '', 100000);
-                    $errors = TRUE;
+            $errors = FALSE;
+            if (empty($_POST['email'])) {
+                // Выдаем куку на день с флажком об ошибке в поле fio.
+                setcookie('email_error', '1', time() + 24 * 60 * 60);
+                $errors = TRUE;
+            }
+            else {
+                // Сохраняем ранее введенное в форму значение на месяц.
+                setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60);
+            }
+            
+            if (($_POST['year'] < 1922) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
+                setcookie('year_error', '1', time() + 24 * 60 * 60);
+                $errors = TRUE;
+            } else {
+                // Сохраняем ранее введенное в форму значение на месяц.
+                setcookie('year_value', $_POST['year'], time() + 30 * 24 * 60 * 60);
+            }
+            if (empty($_POST['gender'])) {
+                setcookie('gender_error', '1', time() + 24 * 60 * 60);
+                $errors = TRUE;
+            } else {
+                // Сохраняем ранее введенное в форму значение на месяц.
+                setcookie('gender_value', $_POST['gender'], time() + 30 * 24 * 60 * 60);
+            }
+            if (empty($_POST['superpower'])) {
+                setcookie('superpower_error', '1', time() + 24 * 60 * 60);
+                $errors = TRUE;
+            } else {
+                // Сохраняем ранее введенное в форму значение на месяц.
+                setcookie('superpower_value', $_POST['superpower'], time() + 30 * 24 * 60 * 60);
+            }
+            
+            if (empty($_POST['text'])) {
+                setcookie('text_error', '1', time() + 24 * 60 * 60);
+                $errors = TRUE;
+            } else {
+                // Сохраняем ранее введенное в форму значение на месяц.
+                setcookie('text_value', $_POST['text'], time() + 30 * 24 * 60 * 60);
+            }
+            if (empty($_POST['limbs'])) {
+                setcookie('limbs_error', '1', time() + 24 * 60 * 60);
+                $errors = TRUE;
+            } else {
+                // Сохраняем ранее введенное в форму значение на месяц.
+                setcookie('limbs_value', $_POST['limbs'], time() + 30 * 24 * 60 * 60);
+                //checked( 'limbs_value', $_POST['limbs'], 'limbs_value' );
+            }
+            if(empty($_SESSION['login'])){
+                if(!isset($_POST['check'])){
+                    setcookie('check_error','1',time()+ 24*60*60);
+                    setcookie('check_value', '', 100000);
+                    $errors=TRUE;
                 }
-                
-                if ($year=='Год') {
-                    setcookie('year_error', '1', time() + 24 * 60 * 60);
-                    setcookie('year_value', '', 100000);
-                    $errors = TRUE;
+                else{
+                    setcookie('check_value',TRUE,time()+ 60*60);
+                    setcookie('check_error','',100000);
                 }
-                
-                if (!isset($pol)) {
-                    setcookie('gender_error', '1', time() + 24 * 60 * 60);
-                    setcookie('gender_value', '', 100000);
-                    $errors = TRUE;
-                }
-                
-                if (!isset($limbs)) {
-                    setcookie('limbs_error', '1', time() + 24 * 60 * 60);
-                    setcookie('limbs_value', '', 100000);
-                    $errors = TRUE;
-                }
-                
-                if (!isset($powers)) {
-                    setcookie('superpower_error', '1', time() + 24 * 60 * 60);
-                    $errors = TRUE;
-                }
-                
-                if ((empty($bio)) || (!preg_match($bioregex,$bio))) {
-                    setcookie('bio_error', '1', time() + 24 * 60 * 60);
-                    setcookie('bio_value', '', 100000);
-                    $errors = TRUE;
-                }
-                
+            }
                 if ($errors) {
                     setcookie('save','',100000);
                     header('Location: index.php?edit_id='.$id);
@@ -242,26 +256,36 @@ if (empty($_SERVER['PHP_AUTH_USER']) ||
                 if(!$errors){
                     $upd=$db->prepare("UPDATE form SET name=:name, email=:email, year=:byear, pol=:pol, limbs=:limbs, bio=:bio WHERE id=:id");
                     $cols=array(
-                        ':name'=>$name,
-                        ':email'=>$email,
-                        ':byear'=>$year,
-                        ':pol'=>$pol,
-                        ':limbs'=>$limbs,
-                        ':bio'=>$bio
+                        ':name'=>$_POST['fio'],
+                        ':email'=>$_POST['email'],
+                        ':byear'=>$_POST['year'],
+                        ':pol'=>$_POST['gender'],
+                        ':limbs'=>$_POST['limbs'],
+                        ':bio'=>$_POST['text']
                     );
+                    foreach($cols as $k=>&$v){
+                        $upd->bindParam($k,$v);
+                    }
                     $upd->bindParam(':id',$id);
                     $upd->execute();
                     $del=$db->prepare("DELETE FROM Sform WHERE id_per=?");
                     $del->execute(array($id));
-                    $upd1=$db->prepare("INSERT INTO Sform SET id_sup=:power,per_id=:id");
-                    $upd1->bindParam(':id',$id);
+                    $stmt = $db->prepare("INSERT INTO Sform SET id_per = ?, id_sup = ?");
+                    if ($_POST['superpower']=='t')
+                    {$stmt -> execute([$id, 10]);}
+                    else if ($_POST['superpower']=='b')
+                    {$stmt -> execute([$id, 20]);}
+                    else if ($_POST['superpower']=='c')
+                    {$stmt -> execute([$id, 30]);}
+                    else if ($_POST['superpower']=='p')
+                    {$stmt -> execute([$id, 40]);}
                 }
                 
                 if(!$errors){
                     setcookie('save', '1');
                 }
                 header('Location: index.php?edit_id='.$id);
-         }
+        }
         else {
             $id=$_POST['dd'];
             $user = 'u52813';
@@ -283,4 +307,3 @@ if (empty($_SERVER['PHP_AUTH_USER']) ||
         }
         
     }
-    
